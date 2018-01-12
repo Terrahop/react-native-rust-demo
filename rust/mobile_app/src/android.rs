3acbb8f518c76339958377b151d7be1f67a08206
Greg Linklater <greglink49@gmail.com>
2018-01-12 14:54:17 +0200

    refactor(mobile_app): clean up interface

--- rust/mobile_app/src/android.rs
+++ rust/mobile_app/src/android.rs
@@ -8,12 +8,14 @@ extern crate untrusted;
 
 use self::jni::JNIEnv;
 use self::jni::objects::{JClass,JString};
-use self::jni::sys::{jstring,jboolean};
-use self::ring::{rand,signature,digest};
-use self::ring::signature::{VerificationAlgorithm};
+use self::jni::sys::{jstring, jboolean};
+use self::ring::{rand,digest};
+use self::ring::signature::{VerificationAlgorithm, Ed25519KeyPair, EdDSAParameters};
 use self::base64::{encode,decode};
+use self::untrusted::Input;
 
 #[no_mangle]
+#[allow(non_snake_case)]
 pub unsafe extern fn Java_com_demo_MobileAppBridge_helloWorld(
   env: JNIEnv,
   _: JClass,
@@ -25,6 +27,7 @@ pub unsafe extern fn Java_com_demo_MobileAppBridge_helloWorld(
 }
 
 #[no_mangle]
+#[allow(non_snake_case)]
 pub unsafe extern fn Java_com_demo_MobileAppBridge_sha256(
   env: JNIEnv,
   _: JClass,
@@ -38,13 +41,13 @@ pub unsafe extern fn Java_com_demo_MobileAppBridge_sha256(
   ctx.update(&bytes[..]);
   let result = ctx.finish();
   let result_bytes = result.as_ref();
-  let encoded = encode(&result_bytes);
 
-  let response = format!("sha256({}): {:?}", data, encoded);
-  env.new_string(response).unwrap().into_inner()
+  let encoded = encode(&result_bytes);
+  env.new_string(encoded).unwrap().into_inner()
 }
 
 #[no_mangle]
+#[allow(non_snake_case)]
 pub unsafe extern fn Java_com_demo_MobileAppBridge_ed25519GeneratePrivateKey(
   env: JNIEnv,
   _: JClass
@@ -52,13 +55,15 @@ pub unsafe extern fn Java_com_demo_MobileAppBridge_ed25519GeneratePrivateKey(
 
   // Generate a key pair in PKCS#8 (v2) format.
   let rng = rand::SystemRandom::new(); //ceate new random number variable
-  let pkcs8_bytes = signature::Ed25519KeyPair::generate_pkcs8(&rng).expect("Failed to generate key");
+
+  let pkcs8_bytes = Ed25519KeyPair::generate_pkcs8(&rng).expect("Failed to generate key");
+
   let encoded = encode(&pkcs8_bytes.as_ref());
   env.new_string(encoded).unwrap().into_inner()
-
 }
 
 #[no_mangle]
+#[allow(non_snake_case)]
 pub unsafe extern fn Java_com_demo_MobileAppBridge_ed25519GetPublicKey(
   env: JNIEnv,
   _: JClass,
@@ -67,39 +72,43 @@ pub unsafe extern fn Java_com_demo_MobileAppBridge_ed25519GetPublicKey(
 
   let key: String = env.get_string(key).unwrap().into();
   let pkcs8_bytes = decode(&key).expect("Invalid Base64");
-  let keypair = signature::Ed25519KeyPair::from_pkcs8(untrusted::Input::from(&pkcs8_bytes.as_ref())).expect("Failed to parse pkcs8");
+
+  let keypair = Ed25519KeyPair::from_pkcs8(Input::from(&pkcs8_bytes.as_ref())).expect("Failed to parse pkcs8");
   let public_bytes = keypair.public_key_bytes();
+
   let encoded = encode(&public_bytes);
   env.new_string(encoded).unwrap().into_inner()
 }
 
 #[no_mangle]
+#[allow(non_snake_case)]
 pub unsafe extern fn Java_com_demo_MobileAppBridge_ed25519Sign(
   env: JNIEnv,
   _: JClass,
   key: JString,
   data: JString
 ) -> jstring  {
-
   let key: String = env.get_string(key).unwrap().into();
-  let data: String = env.get_string(data).unwrap().into();;
+  let data: String = env.get_string(data).unwrap().into();
   let pkcs8_bytes = decode(&key).expect("Invalid Base64");
   let decoded_bytes = decode(&data).expect("Invalid Base64");
-  let keypair = signature::Ed25519KeyPair::from_pkcs8(untrusted::Input::from(&pkcs8_bytes.as_ref())).expect("Failed to parse pkcs8");
-  let signature = keypair.sign(&decoded_bytes);
-  let encoded = encode(&signature);
-  env.new_string(encoded).unwrap().into_inner()
 
+  let keypair = Ed25519KeyPair::from_pkcs8(Input::from(&pkcs8_bytes.as_ref())).expect("Failed to parse pkcs8");
+  let signature = keypair.sign(&decoded_bytes.as_ref());
+
+  let encoded = encode(signature.as_ref());
+  env.new_string(encoded).unwrap().into_inner()
 }
 
 #[no_mangle]
+#[allow(non_snake_case)]
 pub unsafe extern fn Java_com_demo_MobileAppBridge_ed25519Verify(
   env: JNIEnv,
   _: JClass,
   key: JString,
   msg: JString,
   sig: JString
-) -> jstring {
+) -> jboolean {
 
   let key: String = env.get_string(key).unwrap().into();
   let msg: String = env.get_string(msg).unwrap().into();
@@ -109,17 +118,15 @@ pub unsafe extern fn Java_com_demo_MobileAppBridge_ed25519Verify(
   let msg_decoded = decode(&msg).expect("Invalid Base64");
   let sig_decoded = decode(&sig).expect("Invalid Base64");
 
-  let eddsa = signature::EdDSAParameters;
+  let eddsa = EdDSAParameters;
   let verified = eddsa.verify(
-    untrusted::Input::from(&key_decoded),
-    untrusted::Input::from(&msg_decoded),
-    untrusted::Input::from(&sig_decoded)
+    Input::from(&key_decoded.as_ref()),
+    Input::from(&msg_decoded.as_ref()),
+    Input::from(&sig_decoded.as_ref())
   );
 
-  let output = match verified {
+  match verified {
     Ok(_) => 0x01u8,
     Err(_) => 0x00u8,
-  };
-  let response = format!("{:?}", output);
-  env.new_string(response).unwrap().into_inner()
+  }
 }
