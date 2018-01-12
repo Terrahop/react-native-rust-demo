d6bd0f41b89215f379341b506030bba506dc53b4
Greg Linklater <greglink49@gmail.com>
2018-01-12 14:54:37 +0200

    refactor(App): show all steps of eddsa verification

--- App.js
+++ App.js
@@ -21,13 +21,28 @@ async function displayHelloWorld (self) {
       ios: 'iOS',
       android: 'Android',
     })
+
     const greeting = await MobileAppBridge.sayHelloWorld(name)
-    const message = await MobileAppBridge.hashSHA256(greeting)
-    const reply = await MobileAppBridge.verifyWithEd25519('HeyFromReact')
+    const hashed = await MobileAppBridge.hashSHA256(greeting)
+    const privateKey = await MobileAppBridge.generateKey()
+    console.log('PVT', privateKey)
+    const publicKey = await MobileAppBridge.calculatePublicKey(privateKey)
+    console.log('PUB', publicKey)
+    const signature = await MobileAppBridge.sign(privateKey, name)
+    console.log('SIG', signature)
+    const reply = await MobileAppBridge.verify(publicKey, name, signature)
+    console.log('RES_POS', reply)
+    const neg_reply = await MobileAppBridge.verify(publicKey, "Invalid", signature)
+    console.log('RES_NEG', reply)
 
     self.setState({
       ...self.state,
-      message,
+      message: `sha256(${greeting}): ${hashed}`,
+      name,
+      privateKey,
+      publicKey,
+      signature,
+      neg_reply,
       reply
     })
   } catch (e) {
@@ -37,12 +52,17 @@ async function displayHelloWorld (self) {
 
 export default class App extends Component<{}> {
   componentDidMount () {
-    console.log(NativeModules.RustRN)
+    console.log(NativeModules.MobileAppBridge)
     displayHelloWorld(this)
   }
 
   state = {
     message: '',
+    name: '',
+    privateKey: '',
+    publicKey: '',
+    signature: '',
+    neg_reply: '',
     reply: ''
   }
 
@@ -56,10 +76,19 @@ export default class App extends Component<{}> {
           {this.state.message}
         </Text>
         <Text style={styles.instructions}>
-          Hey from React!
+          Private Key: {this.state.privateKey}
         </Text>
         <Text style={styles.instructions}>
-          {this.state.reply}
+          Public Key: {this.state.publicKey}
+        </Text>
+        <Text style={styles.instructions}>
+          Sign({this.state.name}): {this.state.signature}
+        </Text>
+        <Text style={styles.instructions}>
+          Verification Result: {this.state.reply ? "Signature Verified" : "Failed to Verify"}
+        </Text>
+        <Text style={styles.instructions}>
+          Negative Verification Result (should fail): {this.state.neg_reply ? "Signature Verified" : "Failed to Verify"}
         </Text>
       </View>
     )
